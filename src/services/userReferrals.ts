52d16fe009c63b4416ec18bc0200cdb1f78483df
SDiegoFelipe7 <diegofelipem99@hotmail.com>
2023-05-17 18:36:49 -0500

    Cambio

--- src/services/userReferrals.ts
+++ src/services/userReferrals.ts
@@ -1,17 +1,19 @@
+import { AxiosRequestConfig } from "axios";
 import getAxiosClient from "../config/axiosClient";
 import { Referral } from "../types";
 
 const getUserReferrals = async (token: string): Promise<Referral[]> => {
   const axiosClient = getAxiosClient("evoxAPI");
-  const config = {
+  const config: AxiosRequestConfig = {
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${token}`,
+      //"Access-Control-Allow-Origin": "http://localhost:5173", // Agrega este encabezado
     },
   };
   let response: Referral[] = [];
   try {
-    const { data } = await axiosClient("/auth/users/unilevel", config);
+    const { data } = await axiosClient.get("/api/users/unilevel", config);
     response = data;
   } catch (error) {
     console.log(error);
