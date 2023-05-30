baa9384b8b21c92cb7a2b1e42c1687545b628ba6
AlejandroDeveloper2 <90485793+AlejandroDeveloper2@users.noreply.github.com>
2023-05-29 21:22:12 -0500

    Validación username

--- src/pages/userRegister/validationSchema.ts
+++ src/pages/userRegister/validationSchema.ts
@@ -8,6 +8,7 @@ const passwordRulesMessage =
   "La contraseña debe tener al menos una letra mayuscula, una minuscula y un numero!";
 const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}$/;
 const userNameRules = /^(?=.*\d)(?=.*[a-z]).{5,}$/;
+const userNameRule2 = /^[a-zA-Z0-9 ]+$/;
 
 export const validationSchema = yup.object().shape({
   email: yup.string().required(isRequiredMessage).email(validEmailMessage),
@@ -26,6 +27,9 @@ export const validationSchema = yup.object().shape({
     .min(3, "El nombre de usuario debe tener al menos 3 caracteres!")
     .matches(userNameRules, {
       message: "El nombre de usuario debe contener al menos un número!",
+    })
+    .matches(userNameRule2, {
+      message: "El nombre de usuario solo puede contener letras y números!",
     }),
   fullName: yup.string().required(isRequiredMessage),
   phone: yup.string().required(isRequiredMessage),
