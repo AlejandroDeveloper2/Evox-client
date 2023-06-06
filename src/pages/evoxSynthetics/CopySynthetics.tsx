b14f92915dab75034753d7ab2c78c375018bd41a
SDiegoFelipe7 <diegofelipem99@hotmail.com>
2023-06-05 20:28:23 -0500

    Ejemplo

--- src/pages/evoxSynthetics/CopySynthetics.tsx
+++ src/pages/evoxSynthetics/CopySynthetics.tsx
@@ -16,9 +16,7 @@ const CopySynthetics = (): JSX.Element => {
   const { data: status } = useSWR("/synthetic/accountStatus", () =>
     getAccountStatus(token)
   );
-  const { data: transaction } = useSWR("/synthetic/transaction", () =>
-    getTransactionStatus(token)
-  );
+
 
   return (
     <div className="relative flex flex-col pt-20 pb-10 items-center gap-10 px-5  md:px-20">
@@ -38,11 +36,7 @@ const CopySynthetics = (): JSX.Element => {
           24 horas tu cuenta será activada!
         </p>
       ) : status === "Error" ? (
-        <Navigate
-          to={`/dashboard/evoxSynthetics/syntheticsPayment/${JSON.stringify(
-            transaction ? transaction : {}
-          )}`}
-        />
+        <Navigate to={`/dashboard/evoxSynthetics/syntheticsPayment/error`} />
       ) : (
         <LinkAccount />
       )}
